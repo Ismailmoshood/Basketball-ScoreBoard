3cdb5ea24f0b17f582b1d9bea81b3784e9c0be48
Ismailmoshood <114508254+Ismailmoshood@users.noreply.github.com>
2022-11-18 06:49:48 +0100

    added lead checker

--- script.js
+++ script.js
@@ -6,11 +6,13 @@ let result1 = 0;
 let calculate1=(number)=>{
    result1 =result1 + number;
    resultEL1.innerHTML = result1;
+   lead1()
 }
 let result2 = 0
 let calculate2=(number)=>{
    result2 =result2+ number;
    resultEL2.innerHTML = result2;
+   lead2()
 }
 
 
@@ -19,20 +21,21 @@ function reset(){
    resultEL2.innerHTML = 0;
    result1 = 0;
    result2 = 0;
+   lead1()
+   lead2()
 }
-// function lead(){
-//    if(result1 >= result2){
-//       document.getElementById("hscore").style.color = "green" ;
-//    }
-//    // document.getElementById("hscore").style.color = "red";
-// }
-//    // if(result1 < result2){
-//    //    document.getElementById("hscore").style.color = "red" ;
-//    // }
-//    // if(result2 >= result1){
-//    //    document.getElementById("gscore").style.color = "green" ;
-//    // }
-//    // if(result2 < result1){
-//    //    document.getElementById("gscore").style.color = "red" ;
-//    // }
+function lead1(){
+   if(result1 >= result2){
+      document.getElementById("hscore").style.color = "green" ;
+   }else if(result1 < result2){
+      document.getElementById("hscore").style.color = "red" ;
+   }
+   }
+   function lead2(){
+       if(result2 >= result1){
+      document.getElementById("gscore").style.color = "green" ;
+   }else if(result2 < result1){
+      document.getElementById("gscore").style.color = "red" ;
+   }
+}
  
